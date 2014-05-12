2f0373f0f092a581950eabfcc95d4e657fa3257d
kristenmills <kristen@kristen-mills.com>
2014-05-11 23:51:49 -0400

    Memberships now save

--- app/controllers/memberships.rb
+++ app/controllers/memberships.rb
@@ -1,10 +1,24 @@
 Scoreboard::App.controllers :memberships,  conditions: {authorize: true} do
+  get :index, map: '/memberships' do
+    render 'memberships/index'
+  end
 
   get :new, map: '/memberships/new' do
     render 'memberships/new'
   end
 
   post :create, map: '/memberships' do
+    Member.create(params[:member]) # incase it doesn't exist
+    @membership = Membership.new(params[:membership])
+    @membership.member = Member.find_by_dce(params[:member][:dce])
+    @membership.semester = Semester.current_semester
+
+    if @membership.save
+      flash[:notice] = 'Membership was successfully created'
+      redirect_to 'memberships'
+    else
+      render 'memberships/new'
+    end
   end
 
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,4 +2,6 @@ class Membership < ActiveRecord::Base
   belongs_to :member
   belongs_to :semester
   belongs_to :committee
+
+  validates :semester_id, :committee_id, :member_id, :reason, presence: true
 end
