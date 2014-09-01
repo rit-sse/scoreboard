4574c133064e34d4f635085a05e78047fa207313
kristenmills <kristen@kristen-mills.com>
2014-09-01 14:54:57 -0400

    Updated icons

--- app/assets/js/common/toolbar.controller.js
+++ app/assets/js/common/toolbar.controller.js
@@ -7,19 +7,23 @@
     $scope.logged_in_links = [
       {
         url: '/scoreboard/committees/new',
-        content: 'Add Committee'
+        content: 'Add Committee',
+        class: 'fa-tag'
       },
       {
         url: '/scoreboard/semesters/new',
-        content: 'Add Semester'
+        content: 'Add Semester',
+        class: 'fa-calendar'
       },
       {
         url: '/scoreboard/memberships/new',
-        content: 'Add Membership'
+        content: 'Add Membership',
+        class: 'fa-user'
       },
       {
         url: '/scoreboard/memberships',
-        content: 'Membership Lists'
+        content: 'Membership Lists',
+        class: 'fa-users'
       }
     ];
     $scope.logged_out_links = [
